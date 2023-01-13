8e9ad4db5dbd176b05639b01b9e858f346be22c9
SMahdiFaghih <smahdifaghih2001@gmail.com>
2023-01-13 12:41:37 +0330

    Refactor State or Strategy for GrammarSymbol class

--- src/main/java/parser/Rule.java
+++ src/main/java/parser/Rule.java
@@ -8,17 +8,24 @@ import java.util.ArrayList;
  * Created by mohammad hosein on 6/25/2015.
  */
 
-public class Rule {
-    public Rule(String stringRule) {
+public class Rule
+{
+    public Rule(String stringRule)
+    {
         int index = stringRule.indexOf("#");
-        if (index != -1) {
-            try {
+        if (index != -1)
+        {
+            try
+            {
                 semanticAction = Integer.parseInt(stringRule.substring(index + 1));
-            } catch (NumberFormatException ex) {
+            } catch (NumberFormatException ex)
+            {
                 semanticAction = 0;
             }
             stringRule = stringRule.substring(0, index);
-        } else {
+        }
+        else
+        {
             semanticAction = 0;
         }
         String[] splited = stringRule.split("->");
@@ -28,12 +35,16 @@ public class Rule {
 //            e.printStackTrace();
 //        }
         RHS = new ArrayList<GrammarSymbol>();
-        if (splited.length > 1) {
+        if (splited.length > 1)
+        {
             String[] RHSs = splited[1].split(" ");
-            for (String s : RHSs) {
-                try {
+            for (String s : RHSs)
+            {
+                try
+                {
                     RHS.add(new GrammarSymbol(NonTerminal.valueOf(s)));
-                } catch (Exception e) {
+                } catch (Exception e)
+                {
 //                    try{
                     RHS.add(new GrammarSymbol(new Token(Token.getTyepFormString(s), s)));
 //                    }catch (IllegalArgumentException d){
@@ -50,18 +61,42 @@ public class Rule {
     public int semanticAction;
 }
 
-class GrammarSymbol {
-    public boolean isTerminal;
-    public NonTerminal nonTerminal;
-    public Token terminal;
+class GrammarSymbol
+{
+    public GrammarSymbolType grammarSymbolType;
 
-    public GrammarSymbol(NonTerminal nonTerminal) {
-        this.nonTerminal = nonTerminal;
-        isTerminal = false;
+    public GrammarSymbol(NonTerminal nonTerminal)
+    {
+        grammarSymbolType = new NonTerminalGrammarSymbol(nonTerminal);
     }
 
-    public GrammarSymbol(Token terminal) {
+    public GrammarSymbol(Token terminal)
+    {
+        grammarSymbolType = new TerminalGrammarSymbol(terminal);
+    }
+}
+
+class GrammarSymbolType
+{
+
+}
+
+class NonTerminalGrammarSymbol extends GrammarSymbolType
+{
+    public NonTerminal nonTerminal;
+
+    public NonTerminalGrammarSymbol(NonTerminal nonTerminal)
+    {
+        this.nonTerminal = nonTerminal;
+    }
+}
+
+class TerminalGrammarSymbol extends GrammarSymbolType
+{
+    public Token terminal;
+
+    public TerminalGrammarSymbol(Token terminal)
+    {
         this.terminal = terminal;
-        isTerminal = true;
     }
 }
